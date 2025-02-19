73a0dcff2750f38b2295440a5ee8680f06f07c94
VladimirFerraz <vladmesquita@hotmail.com>
2025-02-19 22:54:16 +0000

    Criando carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,10 @@ int main() {
    
     int codigodacidade1, numerodepontosturisticos1, codigodacidade2, numerodepontosturisticos2, populaçao1, populaçao2;
     float area1, PIB1, area2, PIB2;
-    char nome1[50], nome2[50];
+    char cidade1[50], cidade2[50];
 
     printf("Digite o nome da 1º carta: \n");
-    scanf("%s", &nome1);
+    scanf("%s", &cidade1);
 
     printf("Digite o código da cidade da 1º carta: \n");
     scanf("%d", &codigodacidade1);
@@ -29,7 +29,7 @@ int main() {
 
 
     printf("Digite o nome da 2º carta: \n");
-    scanf("%s", &nome2);
+    scanf("%s", &cidade2);
 
     printf("Digite o código da cidade da 2º carta: \n");
     scanf("%d", &codigodacidade2);
@@ -49,7 +49,7 @@ int main() {
 
     printf("------CARTA %d------", codigodacidade1);
     printf("O código da cidade é: %d.\n", codigodacidade1);
-    printf("O nome da cidade é: %s.", nome1);
+    printf("O nome da cidade é: %s.", cidade1);
     printf("A população da cidade é: %d habitante.\n", populaçao1);
     printf("A área da cidade é: %f Km².\n", area1);
     printf("O PIB da cidade é: %f, reais.\n", PIB1);
@@ -57,7 +57,7 @@ int main() {
 
     printf("------CARTA %d------", codigodacidade2);
     printf("O código da cidade é: %d.\n", codigodacidade2);
-    printf("O nome da cidade é: %s.", nome2);
+    printf("O nome da cidade é: %s.", cidade2);
     printf("A população da cidade é: %d habitante.\n", populaçao2);
     printf("A área da cidade é: %f Km².\n", area2);
     printf("O PIB da cidade é: %f, reais.\n", PIB2);

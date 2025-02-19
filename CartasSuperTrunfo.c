064e5dc000658ae3334d21a7ac5c0d63d1584177
VladimirFerraz <vladmesquita@hotmail.com>
2025-02-19 14:37:03 +0000

    Criei um printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,10 @@
 //Teste larissa
 
 int main() {
+    
+    printf("Desafio - Criar cartas");
+    
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     

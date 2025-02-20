9223dcbcf27546f936c1047e28e7047b5ae2b9bc
VladimirFerraz <vladmesquita@hotmail.com>
2025-02-20 13:42:35 +0000

    Criando cartas2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,13 @@
 int main() {
     
    
-    int , numerodepontosturisticos1, , numerodepontosturisticos2, populacao1, populacao2;
+    int numerodepontosturisticos1, numerodepontosturisticos2, populacao1, populacao2;
     float area1, PIB1, area2, PIB2;
-    char cidade1[50], cidade2[50],codigodacidade1,codigodacidade2;
+    char cidade1[50], cidade2[50], codigodacidade1[50], codigodacidade2[50], estado1[50], estado2[50];
 
+    printf("Digite o estado da 1º carta: \n");
+    scanf("%s", &estado1);
+    
     printf("Digite o nome da 1º carta: \n");
     scanf("%s", &cidade1);
 
@@ -27,6 +30,8 @@ int main() {
     scanf("%d", &numerodepontosturisticos1);
 
 
+    printf("Digite o estado da 2º carta: \n");
+    scanf("%s", &estado2);
 
     printf("Digite o nome da 2º carta: \n");
     scanf("%s", &cidade2);
@@ -47,17 +52,19 @@ int main() {
     scanf("%d", &numerodepontosturisticos2);
 
 
-    printf("------CARTA %d------\n", codigodacidade1);
+    printf("------CARTA %s------\n", estado1);
     printf("O código da cidade é: %s.\n", codigodacidade1);
-    printf("O nome da cidade é: %s.", cidade1);
+    printf("O estado da carta é: %s. \n", estado1);
+    printf("O nome da cidade é: %s. \n", cidade1);
     printf("A população da cidade é: %d habitante.\n", populacao1);
     printf("A área da cidade é: %f Km².\n", area1);
     printf("O PIB da cidade é: %f, reais.\n", PIB1);
     printf("Nº de pontos turístico é: %d.\n", numerodepontosturisticos1);
 
-    printf("------CARTA %d------\n", codigodacidade2);
+    printf("------CARTA %s------\n", estado2);
     printf("O código da cidade é: %s.\n", codigodacidade2);
-    printf("O nome da cidade é: %s.", cidade2);
+    printf("O estado da carta é: %s. \n", estado2);
+    printf("O nome da cidade é: %s. \n", cidade2);
     printf("A população da cidade é: %d habitante.\n", populacao2);
     printf("A área da cidade é: %f Km².\n", area2);
     printf("O PIB da cidade é: %f, reais.\n", PIB2);

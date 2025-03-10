435a367222528b62cbb26d96f1b2c8318b6dec22
VladimirFerraz <vladmesquita@hotmail.com>
2025-03-10 22:35:58 +0000

    salvamento 10/03 4

--- CartaSuperTrunfoMestre(com valores inseridos).c
+++ CartaSuperTrunfoMestre(com valores inseridos).c
@@ -3,43 +3,38 @@
 int main() {
     
    
-    int numerodepontosturisticos1, numerodepontosturisticos2, populacao1, populacao2;
-    float area1, PIB1, area2, PIB2, den1, den2, PIBper1, PIBper2, super1, super2;
-    char cidade1[50], cidade2[50], codigodacidade1 [50], codigodacidade2 [50], estado1[50], estado2[50];
-
-   
-   
-    estado1 = 'TO';
-    cidade1 = "Palmas";
-    codigodacidade1 = "A01";
-    populacao1 = 302692;
-    area1 = 2227329;
-    PIB1 = 10333419000;
-    numerodepontosturisticos1 = 19;
+ 
+    char estado1 [50] = "TO";
+    char cidade1 [50] = "Palmas";
+    char codigodacidade1 [50] = "A01";
+    int populacao1 = 302692;
+    float area1 = 2227329;
+    float PIB1 = 10333419000;
+    int numerodepontosturisticos1 = 19;
 
 
-    estado2 = "GO";
-    cidade2 = "Goiania";
-    codigodacidade2 = "B01";
-    populacao2 = 1437366;
-    area2 = 729296;
-    PIB2 = 55314931955;
-    numerodepontosturisticos2 = 14;
+    char estado2 [50] = "GO";
+    char cidade2 [50] = "Goiania";
+    char codigodacidade2 [50] = "B01";
+    int populacao2 = 1437366;
+    float area2 = 729296;
+    float PIB2 = 55314931955;
+    int numerodepontosturisticos2 = 14;
 
     // Cálculos
-    den1 = populacao1 / area1;
-    den2 = populacao2 / area2;
+    float den1 = populacao1 / area1;
+    float den2 = populacao2 / area2;
 
-    PIBper1 = PIB1 / populacao1;
-    PIBper2 = PIB2 / populacao2;
+    float PIBper1 = PIB1 / populacao1;
+    float PIBper2 = PIB2 / populacao2;
 
-    super1 = (float) populacao1 + area1 + PIB1 + numerodepontosturisticos1 + PIBper1 - den1;
-    super2 = (float) populacao2 + area2 + PIB2 + numerodepontosturisticos2 + PIBper2 - den2;
+    float super1 = populacao1 + area1 + PIB1 + numerodepontosturisticos1 + PIBper1 - den1;
+    float super2 = populacao2 + area2 + PIB2 + numerodepontosturisticos2 + PIBper2 - den2;
 
        
    
-    printf("------CARTA %s------\n", estado1);
-    printf("O código da cidade é: %s.\n", codigodacidade1);
+    printf("---------------------CARTA %s---------------------\n", codigodacidade1);
+    printf("O Estado é: %s.\n", estado1);
     printf("O nome da cidade é: %s.\n", cidade1);
     printf("A população da cidade é: %d habitante.\n", populacao1);
     printf("A área da cidade é: %.2f Km².\n", area1);
@@ -49,8 +44,8 @@ int main() {
     printf("O PIB per capita é: %.2f.\n", PIBper1);
     printf("O super poder da carta 1 é: %.2f. \n\n", super1);
 
-    printf("------CARTA %s------\n", estado2);
-    printf("O código da cidade é: %s.\n", codigodacidade2);
+    printf("---------------------CARTA %s---------------------\n", codigodacidade2);
+    printf("O Estado é: %s.\n", estado2);
     printf("O nome da cidade é: %s.\n", cidade2);
     printf("A população da cidade é: %d habitante.\n", populacao2);
     printf("A área da cidade é: %.2f Km².\n", area2);
